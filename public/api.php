ae52b57b8c2621392a12bb49e02902aa30966497
3axap4eHko <3axap4eHko@gmail.com>
2013-10-08 18:40:41 +0300

    api

--- public/api.php
+++ public/api.php
@@ -7,6 +7,20 @@ if (!isset($_POST['playload']))
     throw new \Exception('No playload found');
 }
 
+function loginAndExecute($login, $password, $command)
+{
+    $ssh = new SSH();
+    $ssh->connect();
+    if ($ssh->auth(SSH::AUTH_PASSWORD,[$login, $password]))
+    {
+        $ssh->exec($command);
+        return true;
+    }
+    return false;
+}
+
+
+
 $data = json_decode($_POST['playload'], true);
 
 /**
@@ -15,28 +29,22 @@ $data = json_decode($_POST['playload'], true);
  */
 preg_match('#/([a-zA-Z0-9-_]+)/([a-zA-Z0-9-_]+)/([a-zA-Z0-9-_/]+)#', $data['ref'], $matches);
 list($refs, $type, $pointer) = $matches;
-
-$ssh = new SSH();
-$ssh->connect();
-if ($ssh->auth(SSH::AUTH_PASSWORD,['ssh-login', 'ssh-password']))
+$commit = $data['after'];
+switch($type)
 {
-    $commit = $data['after'];
-    switch($type)
-    {
-        case 'heads':
-            switch($pointer)
-            {
-                case 'develop':
-                    $ssh->exec('cd /var/www/dev && update.sh ' . $commit);
-                    break;
-                case 'master':
-                    $ssh->exec('cd /var/www/pre-prod && update.sh ' . $commit);
-                    break;
-                default:
-            }
-            break;
-        case 'tags':
-            $ssh->exec('cd /var/www/prod && update.sh ' . $commit);
-            break;
-    }
+    case 'heads':
+        switch($pointer)
+        {
+            case 'develop':
+                loginAndExecute('dev-login', 'dev-password', 'cd /var/www/dev && update.sh');
+                break;
+            case 'master':
+                loginAndExecute('pre-prod-login', 'pre-prod-password', 'cd /var/www/pre-prod && update.sh');
+                break;
+            default:
+        }
+        break;
+    case 'tags':
+        loginAndExecute('prod-login', 'prod-password', 'cd /var/www/prod && update.sh');
+        break;
 }
